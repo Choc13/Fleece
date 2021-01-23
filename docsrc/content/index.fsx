8c8a9c0974c7ef389a8a46d749a12cb08b88508a
Ross Donaldson <gastove@gmail.com>
2021-01-23 11:55:26 -0800

    Propose a few documentation enhancements
    This commit:
    
    - Expands the Codec section for DUs to cover non-data-bearing DUs and DUs with
    type-tags.
    - Adds a new `further-techniques` doc to cover cases like fields that are
    required in JSON but not in an F# data model.
    - Updates the index.
    - Updates the global.json to SDK version 5.0 to allow F#5 scripting syntax.
    - Closes #98

--- docsrc/content/index.fsx
+++ docsrc/content/index.fsx
@@ -1,12 +1,12 @@
 (*** hide ***)
-// This block of code is omitted in the generated HTML documentation. Use 
+// This block of code is omitted in the generated HTML documentation. Use
 // it to define helpers that you do not want to show in the documentation.
 
 (**
 Fleece
 ======
 
-Fleece is a library intended to help with parsing and crafting specific Json without having to write data transfer objects in order to get the expected 
+Fleece is a library intended to help with parsing and crafting specific JSON without having to write data transfer objects in order to get the expected
 representation.
 
 *)
@@ -20,6 +20,7 @@ You can get an overview of the important part of the library by reading the foll
  - [ToJson and OfJson](./to-json-and-of-json.html) are the basic building blocks
  - [Codec](./codec.html) let's you combine both ToJson and OfJson in one declaration
  - [Combinators](./combinators.html) lets you have more control
+ - [Further Techniques](./further-techniques.html) describes ways of solving various JSON-wrangling problems
 
 ### Integration with Web frameworks
 
